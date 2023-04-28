a63eeeeaf5ceba8fc2fd9c93ec4c7e2ddd5ea929
rasool bousaidi <rasoolbousaidi@gmail.com>
2023-04-28 19:40:40 +0330

    refactor directories and seperating csv_exporter

---
+++ crawlers/account.js
@@ -0,0 +1,89 @@
+const { initial_logger, change_logger_label } = require("../helpers/initial.js")
+var logger = initial_logger()
+
+module.exports = {
+    async tv_create_account(page, email, username, password, firstName, lastName){
+        const url = "https://www.tradingview.com/"
+        await page.goto(url, 
+            { waitUntil: 'domcontentloaded' }
+            )
+    
+        await page.waitForSelector('button[aria-label="Open user menu"]');
+        user_menu_btn = await page.$('button[aria-label="Open user menu"]');
+        await user_menu_btn.click()
+        
+        await page.waitForSelector("button[data-name=header-user-menu-sign-in]")
+        sign_in_btn = await page.$("button[data-name=header-user-menu-sign-in]")
+        await sign_in_btn.evaluate((el) => el.click());
+    
+        await page.waitForSelector("div.tv-signin-dialog__inner>div>div>span>span")
+        sign_up_btn = await page.$("div.tv-signin-dialog__inner>div>div>span>span")
+        await sign_up_btn.click()
+    
+        await page.waitForSelector('.js-show-email')
+        email_btn = await page.$(".js-show-email");
+        await email_btn.click()
+    
+        await page.waitForSelector('input[name=username]')
+        await page.focus('input[name=username]')
+        await page.keyboard.type(username)
+        await page.focus('input[name=email]')
+        await page.keyboard.type(email)
+        await page.focus('input[name=password]')
+        await page.keyboard.type(password)
+        await page.waitForSelector('input[name=gdpr]')
+        await (await page.$("input[name=gdpr]")).click()
+    
+        console.log("Waiting for user to resolve captcha.")
+        await page.waitForSelector(".tv-signin-dialog__resend")
+    
+    
+        console.log("Waiting for user to Activate the email")
+        console.log("\x1b[33m%s\x1b[0m", "\tTemporary Passed!")
+    
+        await page.waitForSelector('input[name=first_name]')
+        await page.focus('input[name=first_name]')
+        await page.keyboard.type(first_name)
+    
+        await page.waitForSelector('input[name=last_name]')
+        await page.focus('input[name=last_name]')
+        await page.keyboard.type(last_name)
+    
+        await page.click("input[name=marketing_emails]")
+    
+        return 'Done'
+    },
+
+    async tv_login(page, email, password){
+        logger = change_logger_label(logger, "Login")
+        logger.info("Open user menu")
+        await page.waitForSelector('button[aria-label="Open user menu"]');
+        // user_menu_btn = await page.$('button[aria-label="Open user menu"]');
+        await page.click('button[aria-label="Open user menu"]')
+    
+        logger.info("Click on sign in button")
+        await page.waitForSelector("button[data-name=header-user-menu-sign-in]")
+        sign_in_btn = await page.$("button[data-name=header-user-menu-sign-in]")
+        await sign_in_btn.evaluate((el) => el.click());
+    
+        logger.info("Click on email button")
+        await page.waitForSelector('.js-show-email')
+        email_btn = await page.$(".js-show-email");
+        await email_btn.click()
+    
+        logger.info("Typing email and password")
+        await page.waitForSelector('input[name=username]')
+        await page.focus('input[name=username]')
+        await page.keyboard.type(email)
+        await page.focus('input[name=password]')
+        await page.keyboard.type(password)
+    
+        logger.info("Click on remember me checkbox: disable it")
+        await page.click("input[name=remember]")
+    
+        logger.info("Submiting login form")
+        await page.click(".tv-button__loader")
+    
+        return page
+    }
+}
---
+++ src/csv_exporter.js
@@ -0,0 +1,46 @@
+const { tv_login } = require("../crawlers/account.js")
+const { paper_trading_opener, csv_exporter } = require("../crawlers/tv_operations.js")
+
+const { initial_crawler_config, initial_logger, change_logger_label } = require("../helpers/initial.js")
+var logger = initial_logger()
+
+const email = "desab19561@loongwin.com";
+const password = "f92cafd6-a3d2-4996-8bcd-38b6e08dd0e2123!";
+
+(async () => {
+    logger = change_logger_label(logger, "INITIAL")
+    var page;
+    try{
+        logger.info("Opening browser")
+        page = await initial_crawler_config()
+    }
+    catch(err){
+        logger.error(err.message)
+        process.exit(3)
+    }
+
+    logger = change_logger_label(logger, "URL")
+    const url = "https://www.tradingview.com/"
+    logger.info(`Opening tv url: ${url}`)
+    try{
+        await page.goto(url, { waitUntil: 'domcontentloaded' })
+    }
+    catch(err){
+        logger.error(err.message)
+        process.exit()
+    }
+
+    let signed_in_page;
+    try{
+        signed_in_page = await tv_login(page, email, password)
+        logger.info("Login Successful!")
+    }
+    catch(err){
+        logger.error(`Error in logging in: ${err.message}`)
+        process.exit()
+    }
+
+    let paper_trading_page = await paper_trading_opener(signed_in_page);
+
+    await csv_exporter(paper_trading_page)
+})();
