6e66751fb12e0149a5a20ff3c654b896d633636b
rasool bousaidi <rasoolbousaidi@gmail.com>
2023-05-11 14:40:40 +0330

    complete set-prop and transaction

--- src/tv_set-prop_value.js
+++ src/tv_set-prop_value.js
@@ -1,14 +1,12 @@
-const { create_email } = require("../helpers/cpanel.js");
-const { tv_create_account, tv_login } = require("../crawlers/tv_account.js");
+const { tv_login } = require("../crawlers/tv_account.js");
 const { set_prop, paper_trading_opener, create_initial_transaction } = require("../crawlers/tv_operations.js");
-const { cpanel_verfiy_email } = require("../helpers/cpanel_verfiy_email.js")
+const { initProp_API } = require("../helpers/db.js")
 
 const { initial_crawler_config, initial_logger, change_logger_label } = require("../helpers/initial.js");
 var logger = initial_logger();
-const [email, password] = get_initial_args();
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-// const value = get_initial_args();
+const [email, password, value] = get_initial_args();
 
 
 (async () => {
@@ -18,12 +16,13 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
     logger = change_logger_label(logger, "TV_INITIAL_CRAWLER")
     try{
         logger.info("Opening browser")
-        var [page, browser] = await initial_crawler_config(headless=false, width=980,height=766)
+        var [page, browser] = await initial_crawler_config(headless=false, width=1000,height=850)
     }
     catch(err){
         logger.error(err.message)
         process.exit(3)
     }
+    logger = change_logger_label(logger, "TV_SET_PROP");
 
     // email = "Bernadina_Vins_9e785@sarmayedigital.com";
     // password = "9e78529c-ad28-4ac4-a5bb-9acdb72923f0123!";
@@ -32,10 +31,14 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 
     page = await paper_trading_opener(page);
 
-    res = await set_prop(page, value=5000)
+    res = await set_prop(page, value)
     logger.info(`Response of setting prop: ${res}`)
 
     res = await create_initial_transaction(page)
+    logger.info(`Response of setting prop: ${res}`)
+
+    logger.info(`Sending to DB`)
+    res = await initProp_API(email, value);
     logger.info(`Response of setting prop: ${res}`)
 
     console.log("Done")
@@ -46,11 +49,17 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 function get_initial_args(){
     const username = process.argv.slice(2)[0];
     const password = process.argv.slice(2)[1];
+    const value = process.argv.slice(2)[2];
 
     if(username == undefined || password == undefined){
       console.log('\x1b[31m%s\x1b[0m',`Please enter email and password as an argument correctly\n`) //red
       process.exit();
     }
+
+    if(value == undefined){
+        console.log('\x1b[31m%s\x1b[0m',`Please enter value\n`) //red
+        process.exit();
+    }
   
-    return [username, password];
+    return [username, password, value];
   }
