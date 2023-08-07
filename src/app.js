cd87c3e0d480aa3baafeea8c3a88a833ec1967df
rasool bousaidi <rasoolbousaidi@gmail.com>
2023-08-07 10:26:18 +0330

    first multi browser test

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { initial_logger, change_logger_label } = require("../helpers/initial.js")
 const { create_email, write_to_file, cpanel_verfiy_email } = require("../helpers/cpanel.js");
 const { set_prop_trans } = require("./tv_set-prop_value.js");
 const { export_csv } = require("./tv_csv_exporter.js");
+const { acc_login } = require("./tv_acc_login.js");
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
@@ -22,6 +23,8 @@ const jwt_password = "js28Fh43j9j@*J09"
 const app = express();
 
 const PORT = 8000;
+
+var browsers_dict = [];
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -50,6 +53,58 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ message: 'Error generating token' });
   }
 });
+
+app.post('/account_login', async (req, res) => {
+  // Verify the JWT token in the Authorization header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Missing or invalid token' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    jwt.verify(token, secretKey);
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+  email = req.body.email;
+  password = req.body.password;
+
+  // try {
+  //   const data = fs.readFileSync('local_file_db.text', 'utf8');
+  //   console.log(data);
+  // } catch (err) {
+  //   console.error(err);
+  // }
+  // browsers_dict = [];
+  // data.split("\n").forEach((line) => {
+  //   temp = line.split(" - ")
+  //   data = {
+  //     "username": temp[0],
+  //     "browser": temp[1]
+  //   }
+  //   browsers_dict.push(data)
+  // })
+
+  // browser = browsers_dict[username]
+
+  try{
+    const browser = await acc_login(email, password);
+    browsers_dict.push({
+      "username": email,
+      "browser": browser
+    })
+    console.log(`in login: ${browsers_dict.length}`)
+    return res.send({'data': browser, 'message': "" ,'error': ''});
+  }
+  catch(err){
+    console.log(err)
+    return res.status(500).json({'data': {}, 'message': 'Something went wrong', 'error': err.message});
+    // return res.send({'data': {}, 'message': 'Something went wrong', 'error': err.message});
+  }
+});
+
+
 
 // Endpoint for verifying email
 app.post('/cpanel_verfiy_email', async (req, res) => {
@@ -89,8 +144,19 @@ app.post('/set_prop_trans', async (req, res) => {
   } catch (error) {
     return res.status(401).json({ message: 'Invalid token' });
   }
+  console.log(`in set prop: ${browsers_dict.length}`)
+
+  for(let browser_dict of browsers_dict){
+    if(browser_dict.username == req.body.email){
+      var browser = browser_dict.browser
+    }
+  }
+
+  // }
+  // browser = browsers_dict[req.body.email]
+
   try{
-    const api_response = await set_prop_trans(req.body.email, req.body.password, req.body.value);
+    const api_response = await set_prop_trans(req.body.email, req.body.password, req.body.value, browser);
     return res.send({'data': api_response.data, 'message': api_response.message ,'error': ''});
   }
   catch(err){
