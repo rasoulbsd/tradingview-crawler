e715f1403e3ae86ac6e8e851b5fa5274494ed476
rasool bousaidi <rasoolbousaidi@gmail.com>
2023-08-07 10:26:18 +0330

    first multi browser test

--- src/tv_set-prop_value.js
+++ src/tv_set-prop_value.js
@@ -6,33 +6,10 @@ const { initial_crawler_config, initial_logger, change_logger_label } = require(
 var logger = initial_logger();
 
 module.exports = {
-    async set_prop_trans(email, password, value){
+    async set_prop_trans(email, password, value, browser){
+        page = (await browser.pages())[1]
+
         logger = change_logger_label(logger, "TV_SET_PROP");
-        logger.info("Starting")
-    
-        logger = change_logger_label(logger, "TV_INITIAL_CRAWLER")
-        try{
-            logger.info("Opening browser")
-            var [page, browser] = await initial_crawler_config(headless='new', width=1000,height=850)
-        }
-        catch(err){
-            logger.error(err.message)
-            throw new Error("error in openning browser: set_prop")
-        }
-        logger = change_logger_label(logger, "TV_SET_PROP");
-    
-        // page = await tv_login(page, email, password);
-        try{
-            page = await tv_login(page, email, password)
-            logger.info("Login Successful!")
-            await page.screenshot({path: './screenshots/6-Logn_Successful!.png'});
-        }
-        catch(err){
-            logger.error(`Error in logging in: ${err.message}`)
-            await browser.close()
-            throw new Error("Error in logging in in cpanel: \n"+err.message)
-            // process.exit()
-        }
         page = await paper_trading_opener(page);
     
         res = await set_prop(page, value)
@@ -47,9 +24,9 @@ module.exports = {
     
         console.log("Done")
 
-        await browser.close()
+        // await browser.close()
 
-        return {'message': 'Prop set successfully!', 'data': {'value': value}}
+        // return {'message': 'Prop set successfully!', 'data': {'value': value}}
         // ToDo: Sending to DB
     }
 }
